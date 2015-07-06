7ef26dd03c976b56ea46a2d380a64ca57fcbc030
zbajaja <zakaria.bajaja@gmail.com>
2015-07-06 11:47:36 +0200

    rename packages

--- src/main/java/org/slevin/dao/ItemsDao.java
+++
@@ -1,8 +0,0 @@
-package org.slevin.dao;
-
-import org.slevin.common.Item;
-
-
-public interface ItemsDao extends EntityDao<Item> {
-	
-}
--- src/main/java/org/slevin/dao/OrdersDao.java
+++
@@ -1,7 +0,0 @@
-package org.slevin.dao;
-
-import org.slevin.common.Order;
-
-public interface OrdersDao extends EntityDao<Order>{
-	
-}
